97895b6df04f837a034226a8c21b7118d1a10ecd
Simon Berndtsson <simon.berndtsson@gmail.com>
2017-04-10 16:23:09 +0200

    Added comments to the server code

--- backend/src/base/server.go
+++ backend/src/base/server.go
@@ -5,25 +5,37 @@ import (
     "fmt"
 )
 
+
+/*
+
+*/
 func main() {
   fmt.Printf("Starting server...\n")
+  //Skapar två variabler listener och err
+  //Om allt går bra kommer err vara nil, vilket gör det enkelt att testa
+  //så att det gick bra
   listener, err := net.Listen("tcp",":1337")
-  if err != nil {
+  if err != nil { //Här testas om err inte är nil (alltså, har det blivit fel)
     fmt.Printf("Error starting server: %v\n", err)
   }
-  for {
-    connection, errs := listener.Accept()
-    if errs != nil {
+  for { //Denna loopen körs för evigt
+    //Här skapas två nya variabler, connection och errs, samma som innan
+    // men connection är varje anslutning som inkommer till server
+    // dvs en socket
+    connection, errs := listener.Accept() 
+    if errs != nil { //Här testas igen om det blev något fel
       fmt.Printf("Connection Accept error: %v\n",errs)
-    } else {
+    } else { //om inga fel inträffade, kan vi gå vidare
       fmt.Printf("Connection established: %v\n", connection)
-      go handleConnection()
+      go handleConnection(connection) 
     }
   }
 }
 
-
-func handleConnection() {
-  fmt.Printf("Handle connection whattup\n")
+/*
+    Denna funktionen körs som en goroutine parallellt med main.
+*/
+func handleConnection(conn net.Conn) {
+  fmt.Printf("Handle connection whattup: %v\n", conn)
 }
 
