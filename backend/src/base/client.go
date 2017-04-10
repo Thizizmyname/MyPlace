7e0538916684128fdb65f883ebba8ed02899d3db
Simon Berndtsson <simon.berndtsson@gmail.com>
2017-04-10 16:26:15 +0200

    added comments to client code

--- backend/src/base/client.go
+++ backend/src/base/client.go
@@ -9,11 +9,13 @@ import (
 
 func main() {
   fmt.Printf("Connecting to server..\n")
-  conn, err := net.Dial("tcp","127.0.0.1:1337")
-    if err != nil {
+  //Skapar två nya variabler, conn(ection) och err(or)
+  // err är nil om allt gick bra, annars inte nil
+  conn, err := net.Dial("tcp","127.0.0.1:1337") //localhost och port 1337
+    if err != nil { //om något gick fel 
       //Handle errors
       fmt.Printf("Error, %v", err)
-    } else {
+    } else { //annars fortsätt
       fmt.Printf("Connection established to the server..\n")
       fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
         status, err := bufio.NewReader(conn).ReadString('\n')
