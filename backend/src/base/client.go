3b1eefe0885aecdded7d0c008858e96e1865418e
alexwahlandt <alex.wahlandt@gmail.com>
2017-04-11 13:51:32 +0200

    Server can communicate with a client

--- backend/src/base/client.go
+++ backend/src/base/client.go
@@ -4,25 +4,37 @@ import (
     "net"
     "fmt"
     "bufio"
-    )
+	"os"
+)
 
 
 func main() {
-  fmt.Printf("Connecting to server..\n")
-  //Skapar två nya variabler, conn(ection) och err(or)
-  // err är nil om allt gick bra, annars inte nil
-  conn, err := net.Dial("tcp","127.0.0.1:1337") //localhost och port 1337
+	fmt.Printf("Connecting to server..\n")
+	//Skapar två nya variabler, conn(ection) och err(or)
+	// err är nil om allt gick bra, annars inte nil
+	conn, err := net.Dial("tcp","127.0.0.1:1337") //localhost och port 1337
     if err != nil { //om något gick fel 
-      //Handle errors
-      fmt.Printf("Error, %v", err)
+		//Handle errors
+		fmt.Printf("Error, %v", err)
     } else { //annars fortsätt
-      fmt.Printf("Connection established to the server..\n")
-      fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-        status, err := bufio.NewReader(conn).ReadString('\n')
-        if err != nil {
-          fmt.Print("ERROR ERROR ERROR, CALL GO-CHAN")
-        } else {
-          fmt.Printf("status: %v",status)
-        }
-    }
+		fmt.Printf("Connection established to the server..\n")
+
+		// Loopar för att skriva till servern
+		for{
+			reader := bufio.NewReader(os.Stdin)
+
+			fmt.Print("text to send: " )
+			msg, _ := reader.ReadString('\n')
+			fmt.Fprintf(conn,msg)
+
+			
+
+
+			// Koden nedanför är för att läsa tillbaka från servern till klienten
+			text, _ := bufio.NewReader(conn).ReadString('\n')
+			fmt.Print("Message from server: ", text)
+		
+		}
+	}
+
 }
--- backend/src/base/server.go
+++ backend/src/base/server.go
@@ -3,6 +3,9 @@ package main
 import (
     "net"
     "fmt"
+	//"time"
+	"bufio"
+	"os"
 )
 
 
@@ -10,32 +13,52 @@ import (
 
 */
 func main() {
-  fmt.Printf("Starting server...\n")
-  //Skapar två variabler listener och err
-  //Om allt går bra kommer err vara nil, vilket gör det enkelt att testa
-  //så att det gick bra
-  listener, err := net.Listen("tcp",":1337")
-  if err != nil { //Här testas om err inte är nil (alltså, har det blivit fel)
-    fmt.Printf("Error starting server: %v\n", err)
-  }
-  for { //Denna loopen körs för evigt
-    //Här skapas två nya variabler, connection och errs, samma som innan
-    // men connection är varje anslutning som inkommer till server
-    // dvs en socket
-    connection, errs := listener.Accept() 
-    if errs != nil { //Här testas igen om det blev något fel
-      fmt.Printf("Connection Accept error: %v\n",errs)
-    } else { //om inga fel inträffade, kan vi gå vidare
-      fmt.Printf("Connection established: %v\n", connection)
-      go handleConnection(connection) 
-    }
-  }
+	fmt.Printf("Starting server...\n")
+	//Skapar två variabler listener och err
+	//Om allt går bra kommer err vara nil, vilket gör det enkelt att testa
+	//så att det gick bra
+	listener, err := net.Listen("tcp",":1337")
+	if err != nil { //Här testas om err inte är nil (alltså, har det blivit fel)
+		fmt.Printf("Error starting server: %v\n", err)
+	}
+	for { //Denna loopen körs för evigt
+		//Här skapas två nya variabler, connection och errs, samma som innan
+		// men connection är varje anslutning som inkommer till server
+		// dvs en socket
+		connection, errs := listener.Accept() 
+		if errs != nil { //Här testas igen om det blev något fel
+			fmt.Printf("Connection Accept error: %v\n",errs)
+		} else { //om inga fel inträffade, kan vi gå vidare
+			fmt.Printf("Connection established: %v\n", connection)
+			go handleConnection(connection)
+			go readMsg(connection)
+			go sendMsg(connection)
+					
+		}
+	}
 }
 
 /*
     Denna funktionen körs som en goroutine parallellt med main.
 */
 func handleConnection(conn net.Conn) {
-  fmt.Printf("Handle connection whattup: %v\n", conn)
+	fmt.Printf("Handle connection whattup: %v\n", conn)
 }
 
+func readMsg(conn net.Conn){
+	
+	for{
+	msg,_ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Print(" Message recivied: ", msg)
+	}
+}
+
+func sendMsg(conn net.Conn){
+
+	for{
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("text to send: ")
+		msg, _ :=reader.ReadString('\n')
+		fmt.Fprintf(conn,msg)
+	}
+}
