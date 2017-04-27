21bd21fcdb5f0e59b11481f25ebee16f23daa6de
alexwahlandt <alex.wahlandt@gmail.com>
2017-04-27 14:47:00 +0200

    Moved handler.go and made a testfile

--- backend/src/handler/handler.go
+++ backend/src/handler/handler.go
@@ -36,7 +36,11 @@ func getNewerMessages(roomID string, msgID string) []string{
 }
 
 func joinRoom(roomId string, username string) {
-  //
+// Exempel på hur det kan se ut.	
+//	workingUser = getUser(username)
+//	workingRoom = getRoom(roomId)
+//	workingUser.JoinRoom(workingRoom)
+	
 }
 
 func leaveRoom(username string, roomId string) {
