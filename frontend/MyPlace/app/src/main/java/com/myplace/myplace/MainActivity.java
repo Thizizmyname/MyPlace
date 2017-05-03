ceb526904bc3f8e7b2d30c5afb42572add463b86
Thizizmyname <Alexis.remmers@gmail.com>
2017-05-03 15:52:03 +0200

    Started remaking to look like backend code structs

--- frontend/MyPlace/app/src/main/java/com/myplace/myplace/MainActivity.java
+++ frontend/MyPlace/app/src/main/java/com/myplace/myplace/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
