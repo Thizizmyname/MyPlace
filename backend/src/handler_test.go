50903e0cea7a56d27224826961c09288b593b3de
alexwahlandt <alex.wahlandt@gmail.com>
2017-05-07 16:31:26 +0200

    Test för SignUp, går inte igenom, troligen fel med checkUsername

--- backend/src/handler_test.go
+++ backend/src/handler_test.go
@@ -3,9 +3,23 @@ package main
 import (
 
 	"testing"
-
+	"net"
+	"log"
 )
 
+
+
+
+func establishConnection() *net.TCPConn {
+	tcpLAddr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:1337")
+	conn, err := net.DialTCP("tcp", nil, tcpLAddr) //localhost och port 1337
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conn
+
+}
 
 /*
 00.signup
@@ -16,8 +30,20 @@ side-effect: updates users_db
 */
 
 func TestSignUp(t *testing.T){
-	
+	conn := establishConnection()
+	SignUp("Alex","hej123",conn)
+
+	if SignUp("Alex","lol123",conn) {
+		t.Error("Username allready taken\n")
+	}
 
 	
 	
+}
+
+
+func TestJoinRoom(t *testing.T){
+	
+	
+
 }
